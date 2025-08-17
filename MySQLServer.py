3219d0ed8eb013460d430b15ad9d6274e8949467
Olabode <adesamson0404@gmail.com>
2025-08-17 17:16:00 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,14 +2,15 @@ import mysql.connector
 from getpass import getpass
 from mysql.connector import connect, Error
 
-try:
-    with connect(
-        host = "localhost",
-        user = input("Enter username: "),
-    ) as connection:
-        alx_book_store = "Database 'alx_book_store' created successfully!"
-        with connection.cursor() as cursor:
-            cursor.execute(alx_book_store)
-
-except Error as e:
-    print(e)
+def create_database();
+    try:
+        with connect(
+            host = "localhost",
+            user = input("Enter username: "),
+        ) as connection:
+            alx_book_store = "Database 'alx_book_store' created successfully!"
+            with connection.cursor() as cursor:
+                cursor.execute(alx_book_store)
+    
+    except Error as e:
+        print(e)
