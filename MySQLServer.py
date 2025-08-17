89cfe82d3fc9b97adb2dd1a9235dfec82f9361e3
Olabode <adesamson0404@gmail.com>
2025-08-17 17:09:17 +0100

    Create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host = "localhost",
+        user = input("Enter username: "),
+    ) as connection:
+        alx_book_store = "Database 'alx_book_store' created successfully!"
+        with connection.cursor() as cursor:
+            cursor.execute(alx_book_store)
+
+except Error as e:
+    print(e)
